475a03fb635ee49ea7612d83e565a35677abfc30
somang-lim <somangjuha@gmail.com>
2022-07-16 19:18:43 +0900

    목록

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,15 +1,19 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner sc;
     private int wiseSayingLastIndexId;
+    private List<WiseSaying> wiseSayings;
 
     public App(Scanner sc) {
         this.sc = sc;
         wiseSayingLastIndexId = 0;
+        wiseSayings = new ArrayList<>();
     }
 
     public void run() {
@@ -30,7 +34,18 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
 
-                    System.out.printf("%d번 명언이 등록되었습니다.", id);
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+
+                    wiseSayings.add(new WiseSaying(id, content, author));
+                    break;
+                case "목록" :
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for(int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
+                    }
                     break;
                 case "종료" :
                     System.out.print("명언 SSG가 종료되었습니다.");
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -62,6 +62,26 @@ public class AppTest {
         assertTrue(rs.contains("2번 명언이 등록되었습니다."));
     }
 
+    // 목록
+    @Test
+    public void 등록_후_목록에서_확인할_수_있어야_한다() {
+        String rs = AppTestRunner.run("""
+                등록
+                나의 죽음을 적들에게 알리지 말라
+                이순신
+                등록
+                나에게 불가능이란 없다.
+                나폴레옹
+                목록
+                종료
+                """);
+
+        assertTrue(rs.contains("번호 / 작가 / 명언"));
+        assertTrue(rs.contains("----------------------"));
+        assertTrue(rs.contains("2 / 나폴레옹 / 나에게 불가능이란 없다."));
+        assertTrue(rs.contains("1 / 이순신 / 나의 죽음을 적들에게 알리지 말라"));
+    }
+
     // TEST
     @Test
     void 테스트() {
